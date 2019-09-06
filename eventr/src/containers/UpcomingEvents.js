f8d2d06ea33c01dbce182283b90b43bf19a09189
github username <github email address>
2019-09-06 16:52:12 -0400

    fetch

--- eventr/src/containers/UpcomingEvents.js
+++ eventr/src/containers/UpcomingEvents.js
@@ -3,6 +3,23 @@ import EventCard from '../components/EventCard'
 
 class UpcomingEvents extends React.Component {
 
+
+    componentDidMount(){
+        let data = {
+            category: [102, 103]
+        }
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accepts': 'application.json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(console.log)
+    }
+
     render(){
         return(
             <div>Upcoming Events
